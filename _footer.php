eef3e23605e40c6c5d51f8da716b0582213b0a73
Tobias Zwick <newton@westnordost.de>
2022-12-04 22:05:03 +0100

    dark mode...

--- _footer.php
+++ _footer.php
@@ -3,7 +3,7 @@
 		<div class="navlinks">
 			<div class="linklist">
 				<h3><?php echo $strings["about_title_get_involved"]; ?></h3>
-				<a href="https://github.com/streetcomplete/streetcomplete"><img style="float: left; margin-right: 4px;" width="20" height="20" src="res/logo_github.png" alt="GitHub"><?php echo $strings["about_title_repository"]; ?> (GitHub)</a>
+				<a href="https://github.com/streetcomplete/streetcomplete"><img class="fill_background" style="float: left; margin-right: 4px;" width="20" height="20" src="res/logo_github.png" alt="GitHub"><?php echo $strings["about_title_repository"]; ?> (GitHub)</a>
 				<a href="https://osmus.slack.com/archives/CRCUH32G5"><img style="float: left; margin-right: 4px;" width="20" height="20" src="res/logo_slack.svg" alt="Slack">#streetcomplete chat (Slack)</a>
 				<a href="https://poeditor.com/join/project/IE4GC127Ki"><?php echo $strings["about_title_translate"]; ?></a>
 			</div>
@@ -12,8 +12,8 @@
 				<span><?php echo $strings["about_summary_donate"]; ?></span>
 				<div>
 					<a href="https://liberapay.com/westnordost"><img width="64" height="64" src="res/logo_liberapay.svg" alt="Liberapay"></a>
-					<a href="https://github.com/sponsors/westnordost"><img width="64" height="64" src="res/logo_github.png" alt="GitHub Sponsors"></a>
-					<a href="https://patreon.com/westnordost"><img width="64" height="64" src="res/logo_patreon.png" alt="Patreon"></a>
+					<a href="https://github.com/sponsors/westnordost"><img class="fill_background" width="64" height="64" src="res/logo_github.png" alt="GitHub Sponsors"></a>
+					<a href="https://patreon.com/westnordost"><img class="fill_background" width="64" height="64" src="res/logo_patreon.png" alt="Patreon"></a>
 				</div>
 			</div>
 			<div class="linklist">
