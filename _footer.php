f16c9749b9d177166aa46ffb2988ead3b0ac2c64
Tobias Zwick <newton@westnordost.de>
2024-05-06 00:43:59 +0200

    update logos

--- _footer.php
+++ _footer.php
@@ -3,7 +3,9 @@
 		<div class="navlinks">
 			<div class="linklist">
 				<h3><?php echo $strings["about_title_get_involved"]; ?></h3>
-				<a href="https://github.com/streetcomplete/streetcomplete"><img class="fill_background" style="float: left; margin-right: 4px;" width="20" height="20" src="res/logo_github.png" alt="GitHub"><?php echo $strings["about_title_repository"]; ?> (GitHub)</a>
+				<a href="https://github.com/streetcomplete/streetcomplete"><picture>
+					<source media="(prefers-color-scheme: dark)" srcset="res/logo_github_dark.svg">
+					<img style="float: left; margin-right: 4px;" width="20" height="20" src="res/logo_github.svg" alt="GitHub"></picture><?php echo $strings["about_title_repository"]; ?> (GitHub)</a>
 				<a href="https://osmus.slack.com/archives/CRCUH32G5"><img style="float: left; margin-right: 4px;" width="20" height="20" src="res/logo_slack.svg" alt="Slack">#streetcomplete chat (Slack)</a>
 				<a href="https://poeditor.com/join/project/IE4GC127Ki"><?php echo $strings["about_title_translate"]; ?></a>
 			</div>
@@ -11,9 +13,17 @@
 				<h3><?php echo $strings["about_title_donate"]; ?></h3>
 				<span><?php echo $strings["about_summary_donate"]; ?></span>
 				<div>
-					<a href="https://liberapay.com/westnordost"><img width="64" height="64" src="res/logo_liberapay.svg" alt="Liberapay"></a>
-					<a href="https://github.com/sponsors/westnordost"><img class="fill_background" width="64" height="64" src="res/logo_github.png" alt="GitHub Sponsors"></a>
-					<a href="https://patreon.com/westnordost"><img class="fill_background" width="64" height="64" src="res/logo_patreon.png" alt="Patreon"></a>
+					<a href="https://liberapay.com/westnordost">
+						<img width="64" height="64" src="res/logo_liberapay.svg" alt="Liberapay">
+					</a>
+					<a href="https://github.com/sponsors/westnordost"><picture>
+						<source media="(prefers-color-scheme: dark)" srcset="res/logo_github_dark.svg">
+						<img width="64" height="64" src="res/logo_github.svg" alt="GitHub Sponsors">
+					</picture></a>
+					<a href="https://patreon.com/westnordost"><picture>
+						<source media="(prefers-color-scheme: dark)" srcset="res/logo_patreon_dark.svg">
+						<img width="64" height="64" src="res/logo_patreon.svg" alt="Patreon">
+					</picture></a>
 				</div>
 			</div>
 			<div class="linklist">
